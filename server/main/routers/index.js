82e36976fcf0ac2c9d30baaeb6f7a00ad020572c
Erno <bimonugraraga@gmail.com>
2022-03-17 00:50:46 +0700

    menambahkan authentication, id dan email disimpan dalam req.loggedUser

--- server/main/routers/index.js
+++ server/main/routers/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const authn = require('../middlewares/authn')
 const userRoute = require('./users')
 
 
 //!User Route
 router.use('/users', userRoute)
+
+//!Authn
+router.use(authn())
 
 module.exports = router
