e33e7ce54ebdfc0f1a1f2ef680ac575c4fee3250
joeyregawa <joeyregawa@gmail.com>
2022-03-19 02:15:30 +0700

    detail weather, list weather

--- mobile-client/src/screens/DetailWeather.js
+++ mobile-client/src/screens/DetailWeather.js
@@ -1,15 +1,121 @@
 import react from 'react'
-import { View, Text} from 'react-native'
+import { View,TouchableOpacity} from 'react-native'
 import { Button} from "native-base";
+import { Box, Heading, AspectRatio, Image, Text, Center, HStack, Stack, NativeBaseProvider } from "native-base";
+import {Entypo,MaterialCommunityIcons,Feather} from 'react-native-vector-icons';
 
 export default function DetailWeather({navigation}) {
 
+  const cuaca = {
+    lat: -6.0398,
+    lon: 107.2456,
+    timezone: "Asia/Jakarta",
+    current: {
+      dt: 1647478187,
+      sunrise: 1647471334,
+      sunset: 1647515010,
+      temp: 27.14,
+      feels_like: 29.65,
+      pressure: 1009,
+      humidity: 76,
+      dew_point: 22.54,
+      uvi: 3.1,
+      clouds: 100,
+      visibility: 10000,
+      wind_speed: 2.37,
+      wind_deg: 103,
+      wind_gust: 3.5,
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "awan mendung",
+          icon: "http://openweathermap.org/img/wn/04d@2x.png",
+        },
+      ],
+    },
+  };
+
+  const todayDate = new Date().toLocaleString('en-US', { hour: 'numeric', hour12: true })
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detail Cuaca</Text>
-      <Button mt="2"
-        onPress={() => navigation.navigate('FormCuaca')}
-      >Report Cuaca</Button>
-    </View>
+   
+    <NativeBaseProvider >
+        <Center flex={1} px="3" style ={{backgroundColor : "#fef3c7"}}>
+        <Box alignItems="center" >
+      <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+      borderColor: "coolGray.600",
+      backgroundColor: "gray.700"
+    }} _web={{
+      shadow: 2,
+      borderWidth: 0
+    }} _light={{
+      backgroundColor: "gray.50"
+    }}>
+        <Box style={{backgroundColor: "#22d3ee"}}>
+          <AspectRatio w="100%" >
+            <Image source={{
+            uri: cuaca.current.weather[0].icon
+          }} alt="image" />
+          </AspectRatio>
+          <Center bg="#3f3f46" _dark={{
+          bg: "#3f3f46"
+        }} _text={{
+          color: "warmGray.50",
+          fontWeight: "700",
+          fontSize: "xs"
+        }} position="absolute" bottom="0" px="3" py="1.5">
+            {cuaca.current.weather[0].description}
+          </Center>
+        </Box>
+        <Stack p="4" space={3}>
+          <View style= {{flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              {cuaca.timezone}
+            </Heading>
+            <Text fontSize="xs" _light={{
+            color: "violet.500"
+          }} _dark={{
+            color: "violet.400"
+          }} fontWeight="500" ml="-0.5" mt="-1">
+             {todayDate}
+            </Text>
+          </Stack>
+          <Text style={{fontSize: 22.5, fontWeight: "bold", color: "#1e293b"}}>{cuaca.current.temp}°C</Text>
+          </View>
+          <Text fontWeight="bold" color="#1e293b" marginBottom= "1.5" style= {{textAlign: "center"}}>
+              Terasa {cuaca.current.feels_like}°C. Kondisi: {cuaca.current.weather[0].description}</Text>
+        <View style= {{flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
+          <View style= {{flexDirection: "row", alignItems: "center"}}>
+          <Entypo name = "direction"  />
+          <Text fontWeight="400" style={{marginStart: 5}}>
+            {cuaca.current.wind_speed} m/s WSW
+          </Text>
+          </View>
+          <Entypo name = "air"> <Text fontWeight="400" style={{marginStart: 5}}>
+            {cuaca.current.pressure} hPa
+          </Text></Entypo>
+        </View>
+        <View style= {{flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
+          <Text fontWeight="400" style={{marginStart: 5}}> Jarak Pandang: {(cuaca.current.visibility)/1000} Km </Text>
+          <MaterialCommunityIcons name = "air-humidifier"><Text fontWeight="400" style={{marginStart: 5}}>
+              {cuaca.current.humidity} %</Text> </MaterialCommunityIcons>
+        </View>
+        </Stack>
+          <TouchableOpacity><Button style={{backgroundColor: "#22d3ee"}} mt="2"
+          onPress={() => navigation.navigate('FormCuaca')}
+        >Report Cuaca</Button></TouchableOpacity>
+      </Box>
+    </Box>;
+      </Center>
+    </NativeBaseProvider>
+
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //   <Text>Detail Cuaca</Text>
+    //   <Button mt="2"
+    //     onPress={() => navigation.navigate('FormCuaca')}
+    //   >Report Cuaca</Button>
+    // </View>
   )
 }
--- mobile-client/src/screens/Weather.js
+++ mobile-client/src/screens/Weather.js
@@ -1,15 +1,277 @@
-import react from 'react'
-import { View, Text} from 'react-native'
-import { Button} from "native-base";
+import react from "react";
+import { View,ScrollView,TouchableOpacity,StyleSheet } from "react-native";
+import { Button, Row } from "native-base";
+import React from "react";
+import {
+  Box,
+  Heading,
+  AspectRatio,
+  Image,
+  Text,
+  Center,
+  HStack,
+  Stack,
+  NativeBaseProvider,
+} from "native-base";
 
-export default function Weather({navigation}) {
+import {Entypo} from 'react-native-vector-icons';
 
+export default function Weather({ navigation }) {
+  const cuaca = {
+    lat: -6.0398,
+    lon: 107.2456,
+    timezone: "Asia/Jakarta",
+    current: {
+      dt: 1647478187,
+      sunrise: 1647471334,
+      sunset: 1647515010,
+      temp: 27.14,
+      feels_like: 29.65,
+      pressure: 1009,
+      humidity: 76,
+      dew_point: 22.54,
+      uvi: 3.1,
+      clouds: 100,
+      visibility: 10000,
+      wind_speed: 2.37,
+      wind_deg: 103,
+      wind_gust: 3.5,
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "awan mendung",
+          icon: "http://openweathermap.org/img/wn/04d@2x.png",
+        },
+      ],
+    },
+  };
+  
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> List Cuaca</Text>
-      <Button mt="2"
-        onPress={() => navigation.navigate('DetailCuaca')}
-      >Detail Cuaca</Button>
+    <ScrollView>
+      <View style ={{backgroundColor : "#fef3c7"}}>
+    <NativeBaseProvider>
+      <Text style={{textAlign : "center", marginTop: 10, marginBottom: 5, fontSize: 23, fontWeight: "bold", color: "#525252" }}>Lokasi Saat Ini</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("DetailCuaca")}>
+      <Center px="3" style={styles.lokasisaatini} >
+        <Box alignItems="center" style = {styles.boxcuacasataini}>
+          <Box
+            maxW="80"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _web={{
+              shadow: 2,
+              borderWidth: 0,
+            }}
+            _light={{
+              backgroundColor: "#22d3ee",
+            }}
+          >
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1" color="white">
+                  {cuaca.timezone}
+                </Heading>
+              </Stack>
+              <Box style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <AspectRatio w="30%" ratio={16 / 9}>
+                  <Image
+                    source={{ uri: cuaca.current.weather[0].icon }}
+                    alt="image"
+                  />
+                </AspectRatio>
+                <Text style={{fontSize: 21, fontWeight: "bold", color: "white"}}>{cuaca.current.temp}°C</Text>
+              </Box>
+              <View>
+                <Text fontWeight="bold" color="#f0f9ff" marginBottom= "1.5">
+                  Terasa {cuaca.current.feels_like}°C. Kondisi: {cuaca.current.weather[0].description}
+                </Text>
+              </View>
+            </Stack>
+          </Box>
+        </Box>
+        {/* <Button mt="2" onPress={() => navigation.navigate("DetailCuaca")}>
+          Detail Cuaca
+        </Button> */}
+      </Center>
+      </TouchableOpacity>
+     
+      <Text style={{textAlign : "center", marginBottom: 5, fontSize: 23, fontWeight: "bold", color: "#525252" }}>Lokasi Lain</Text>
+      <Center  px="3" >
+      <TouchableOpacity onPress={() => navigation.navigate("DetailCuaca")}>
+        <Box alignItems="center" style={styles.boxlokasilain}>
+          <Box
+            maxW="80"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            marginBottom= "5"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _web={{
+              shadow: 2,
+              borderWidth: 0,
+            }}
+            _light={{
+              backgroundColor: "#22d3ee",
+            }}
+          >
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1" color="white">
+                  {cuaca.timezone}
+                </Heading>
+              </Stack>
+              <Box style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <AspectRatio w="30%" ratio={16 / 9}>
+                  <Image
+                    source={{ uri: cuaca.current.weather[0].icon }}
+                    alt="image"
+                  />
+                </AspectRatio>
+                <Text style={{fontSize: 21, fontWeight: "bold", color: "white"}}>{cuaca.current.temp}°C</Text>
+              </Box>
+              <View>
+                <Text fontWeight="bold" color="#f0f9ff" marginBottom= "1.5">
+                  Terasa {cuaca.current.feels_like}°C. Kondisi: {cuaca.current.weather[0].description}
+                </Text>
+              </View>
+            </Stack>
+          </Box>
+        </Box>
+        </TouchableOpacity>
+        
+        <Box alignItems="center"  >
+          <Box
+            maxW="80"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            marginBottom="5"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _web={{
+              shadow: 2,
+              borderWidth: 0,
+            }}
+            _light={{
+              backgroundColor: "#22d3ee",
+            }}
+          >
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1" color="white">
+                  {cuaca.timezone}
+                </Heading>
+              </Stack>
+              <Box style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <AspectRatio w="30%" ratio={16 / 9}>
+                  <Image
+                    source={{ uri: cuaca.current.weather[0].icon }}
+                    alt="image"
+                  />
+                </AspectRatio>
+                <Text style={{fontSize: 21, fontWeight: "bold", color: "white"}}>{cuaca.current.temp}°C</Text>
+              </Box>
+              <View>
+                <Text fontWeight="bold" color="#f0f9ff" marginBottom= "1.5">
+                  Terasa {cuaca.current.feels_like}°C. Kondisi: {cuaca.current.weather[0].description}
+                </Text>
+              </View>
+            </Stack>
+          </Box>
+        </Box>
+        
+        <Box alignItems="center"  >
+          <Box
+            maxW="80"
+            rounded="lg"
+            overflow="hidden"
+            borderColor="coolGray.200"
+            borderWidth="1"
+            marginBottom="5"
+            _dark={{
+              borderColor: "coolGray.600",
+              backgroundColor: "gray.700",
+            }}
+            _web={{
+              shadow: 2,
+              borderWidth: 0,
+            }}
+            _light={{
+              backgroundColor: "#22d3ee",
+            }}
+          >
+            <Stack p="4" space={3}>
+              <Stack space={2}>
+                <Heading size="md" ml="-1" color="white">
+                  {cuaca.timezone}
+                </Heading>
+              </Stack>
+              <Box style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <AspectRatio w="30%" ratio={16 / 9}>
+                  <Image
+                    source={{ uri: cuaca.current.weather[0].icon }}
+                    alt="image"
+                  />
+                </AspectRatio>
+                <Text style={{fontSize: 21, fontWeight: "bold", color: "white"}}>{cuaca.current.temp}°C</Text>
+              </Box>
+              <View>
+                <Text fontWeight="bold" color="#f0f9ff" marginBottom= "1.5">
+                  Terasa {cuaca.current.feels_like}°C. Kondisi: {cuaca.current.weather[0].description}
+                </Text>
+              </View>
+            </Stack>
+          </Box>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
     </View>
-  )
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create ({
+  lokasisaatini: {
+    marginBottom: 10,
+    // borderWidth: 2,
+    // borderColor: "rgba(62, 71, 75, 0.97)",
+  
+  },
+  boxcuacasataini: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.43,
+    shadowRadius: 9.51,
+
+    elevation: 15,
+  },
+  boxlokasilain : {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+
+  }
+})
