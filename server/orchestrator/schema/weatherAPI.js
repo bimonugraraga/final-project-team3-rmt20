6de88027cc244bd946d8181b22d648cd0891d0f0
ridhasidi <mulyawan.rdhsd@gmail.com>
2022-03-22 11:28:46 +0700

    automatically read hourly weather forecast at 6am

--- server/orchestrator/schema/weatherAPI.js
+++ server/orchestrator/schema/weatherAPI.js
@@ -1,6 +1,11 @@
 const { gql } = require("apollo-server");
-
 const axios = require("axios");
+const Queue = require("bull");
+const { redis } = require("../config/connectRedis");
+
+const weatherForecast = new Queue("weatherForecast", `redis://:${process.env.REDISPASSWORD}@${process.env.REDISENDPOINT}:${process.env.REDISPORT}`);
+
+// const weatherNotif = new Queue("weatherNotif", `redis://:${process.env.REDISPASSWORD}@${process.env.REDISENDPOINT}:${process.env.REDISPORT}`);
 
 const typeDefs = gql`
   type weatherDetailApi {
@@ -22,6 +27,12 @@ const typeDefs = gql`
     weather: [weatherDetail]
   }
 
+  type alerts {
+    sender_name: String
+    event: String
+    description: String
+  }
+
   type weatherDetail {
     id: Int
     main: String
@@ -30,6 +41,7 @@ const typeDefs = gql`
   }
   extend type Query {
     fetchCurrentWeather(lat: Float, lon: Float): weatherDetailApi
+    weatherNotif(lat: Float, lon: Float): [current]
   }
 `;
 
@@ -47,8 +59,43 @@ const resolvers = {
         return error.response.data;
       }
     },
+
+    weatherNotif: async (_, args) => {
+      try {
+        const { lat, lon } = args;
+        const resp = await axios({ method: "GET", url: baseUrl + `&lat=${lat}&lon=${lon}&exclude=minutely,current,daily` });
+        let data = [];
+        resp.data.hourly.slice(-24).forEach((e) => {
+          if (e.weather[0].main === "Rain" || e.weather[0].main === "Thunderstorm" || e.weather[0].main === "Drizzle") {
+            data.push(e);
+          }
+        });
+        // const result = {
+        //   hourly: data,
+        // };
+        return data;
+      } catch (error) {
+        return error.response.data;
+      }
+    },
   },
 };
+
+weatherForecast.process(async () => {
+  const lat = -6.1753942;
+  const lon = 106.827183;
+  return await resolvers.Query.weatherNotif(lat, lon);
+});
+
+weatherForecast.add(
+  {},
+  {
+    repeat: {
+      // every 6AM
+      cron: `0 6 * * *`,
+    },
+  }
+);
 
 module.exports = {
   typeDefs,
