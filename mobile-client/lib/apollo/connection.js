3b03d00aca18cdb166661e8bc7e5a62b7d9f6866
Erno <bimonugraraga@gmail.com>
2022-03-22 10:55:08 +0700

    push notif

--- mobile-client/lib/apollo/connection.js
+++ mobile-client/lib/apollo/connection.js
@@ -1,7 +1,7 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'https://194d-114-124-208-196.ngrok.io',
+  uri: 'https://26da-202-80-212-109.ngrok.io',
   cache: new InMemoryCache()
 })
 
