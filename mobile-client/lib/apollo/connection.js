3da34a9de318c39f69fdd3925945a484cf9a2a61
joeyregawa <joeyregawa@gmail.com>
2022-03-25 11:04:28 +0700

    deploy

--- mobile-client/lib/apollo/connection.js
+++ mobile-client/lib/apollo/connection.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://final-project-alertme-graphql.herokuapp.com/",
+  uri: "https://final-project-alertme-graphql.herokuapp.com",
   cache: new InMemoryCache(),
 });
 
